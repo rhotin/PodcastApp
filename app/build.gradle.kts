5eabb7ca3df804fb2d787be4ee0f3411222e01bc
r47 <roman.hotin.rh@gmail.com>
2025-01-14 21:33:54 -0500

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+
 }
 
 android {
@@ -56,6 +59,31 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //compose Navigation
+    implementation(libs.androidx.navigation.compose)
+    //coil
+    implementation(libs.coil.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.androidx.hilt.compiler)
+    //Room
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.paging)
+    //paging3
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.paging.compose)
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
 
 
 }
---
+++ app/src/main/java/com/rhappdeveloper/podcastapp/PodcastApp.kt
@@ -0,0 +1,7 @@
+package com.rhappdeveloper.podcastapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PodcastApp : Application()
